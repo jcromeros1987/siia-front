ee6a6e7ff7d0340e8b0d54109b42d27750969e91
geromt <gerardomartinez205@gmail.com>
2026-02-14 18:37:39 -0600

    feat: implement TokenContext and custom hooks for token management

---
+++ src/context/TokenContext.jsx
@@ -0,0 +1,27 @@
+import { createContext } from 'react'
+import { useLocalStorage} from '../hooks/useLocalStorage'
+
+const TokenContext = createContext()
+
+const TokenProvider = ({ children }) => {
+  const [tokenAccess, setTokenAccess] = useLocalStorage('tokenAccess', null)
+  const [tokenRefresh, setTokenRefresh] = useLocalStorage('tokenRefresh', null)
+
+  const token = {
+    access: tokenAccess,
+    refresh: tokenRefresh
+  }
+
+  const setToken = (access, refresh) => {
+    setTokenAccess(access)
+    setTokenRefresh(refresh)
+  }
+
+  return (
+    <TokenContext.Provider value={{ token, setToken }}>
+      {children}
+    </TokenContext.Provider>
+  )
+}
+
+export { TokenContext, TokenProvider }
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+  // Get stored value or use initialValue
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? item : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  // Update localStorage when state changes
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      
+      if (valueToStore === null) {
+        window.localStorage.removeItem(key);
+      } else {
+        window.localStorage.setItem(key, valueToStore);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
