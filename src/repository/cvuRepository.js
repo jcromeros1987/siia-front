6a0918b1b7bb28d20c38c8b8c00cd81c4ede91a2
geromt <gerardomartinez205@gmail.com>
2026-02-15 10:12:32 -0600

    refactor: change async version of fetchCVU to return a promise

--- src/repository/cvuRepository.js
+++ src/repository/cvuRepository.js
@@ -2,11 +2,5 @@ import { createApiClient } from '../api/axiosConfig'
 
 export const fetchCVU = async ({ token, userId }) => {
   const api = createApiClient(token)
-  try {
-    const response = await api.get(`/api/v1/cvu/${userId}`)
-    console.log(response.data)
-    return response.data.data
-  } catch (error) {
-    console.error('Error fetching CVU:', error)
-  }
+  return api.get(`/api/v1/cvu/${userId}`)
 }
