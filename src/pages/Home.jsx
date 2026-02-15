24409fca9d33e319122252060a143199ec9985f6
geromt <gerardomartinez205@gmail.com>
2026-02-15 09:04:03 -0600

    feat: implement data fetching for CVU based on userId from token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from 'react'
+import { useToken } from '@/hooks/useToken'
+import { fetchCVU } from '@/repository/cvuRepository'
+
 const Home = () => {
+  const { token, userId } = useToken()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (userId) {
+        console.log('User ID from token:', userId)
+        const cvuData = await fetchCVU({ token, userId })
+        console.log('CVU data:', cvuData)
+      }
+    }
+
+    fetchData()
+  }, [token, userId])
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold text-primary'>¡Bienvenido a SIIA!</h1>
