ced9aa849d98b3e267bd05ec6058f51e9d014715
geromt <gerardomartinez205@gmail.com>
2026-02-16 19:59:58 -0600

    feat: update fetchCVU and getFormSpecification to support token refresh handling

--- src/repository/cvuRepository.js
+++ src/repository/cvuRepository.js
@@ -1,13 +1,13 @@
 import { createApiClient } from '@/api/axiosConfig'
 
-const fetchCVU = async ({ token, userId }) => {
-  const api = createApiClient(token)
+const fetchCVU = async ({ token, onTokenRefresh, userId }) => {
+  const api = createApiClient(token, onTokenRefresh)
   return api.get(`/api/v1/cvu/${userId}`)
 }
 
-const getFormSpecification = async ({ token, productType }) => {
+const getFormSpecification = async ({ token, onTokenRefresh, productType }) => {
   console.log('Fetching form specification for product type:', productType)
-  const api = createApiClient(token)
+  const api = createApiClient(token, onTokenRefresh)
   return api.get(`/api/v1/cvu/form/${productType}/`)
 }
 
