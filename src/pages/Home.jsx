62eb8647065acbbdd0c7c4dc36a716ed5b034d89
geromt <gerardomartinez205@gmail.com>
2026-02-23 12:23:30 -0600

    feat: restructure Home component to include carousel for UserInfo and CVUInfo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,34 @@
 import { useEffect } from 'react'
 import { useFetchCVU } from '@/hooks/useFetchCVU'
 import UserInfo from '@/components/UserInfo'
+import { CVUInfo } from '@/components/CVUInfo'
 
 const Home = () => {
-  const { userData, fetchCVUData, isLoading } = useFetchCVU()
+  const { userData, cvuData, fetchCVUData, isLoading } = useFetchCVU()
 
   useEffect(() => {
     fetchCVUData()
   }, [])
 
   return (
-    <UserInfo userData={userData} isLoading={isLoading} />
+    <div className='caruousel w-full'>
+      <div id='slide1' className='carousel-item relative w-full'>
+        <UserInfo userData={userData} isLoading={isLoading} />
+        <div className='absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between'>
+          <a href='#slide2' className='btn btn-circle'>❮</a>
+          <a href='#slide2' className='btn btn-circle'>❯</a>
+        </div>
+      </div>
+
+      <div id='slide2' className='carousel-item relative w-full'>
+        <CVUInfo cvuData={cvuData} fetchCVUData={fetchCVUData} isLoading={isLoading} />
+        <div className='absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between'>
+          <a href='#slide1' className='btn btn-circle'>❮</a>
+          <a href='#slide1' className='btn btn-circle'>❯</a>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
