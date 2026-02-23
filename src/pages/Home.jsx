d61ae08b963dc0e778cbccdaefd868d69d1301b0
geromt <gerardomartinez205@gmail.com>
2026-02-23 11:31:22 -0600

    feat: include loading state in UserInfo component for improved user experience

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import { useFetchCVU } from '@/hooks/useFetchCVU'
 import UserInfo from '@/components/UserInfo'
 
 const Home = () => {
-  const { userData, fetchCVUData } = useFetchCVU()
+  const { userData, fetchCVUData, isLoading } = useFetchCVU()
 
   useEffect(() => {
     fetchCVUData()
   }, [])
 
   return (
-    <UserInfo userData={userData} />
+    <UserInfo userData={userData} isLoading={isLoading} />
   )
 }
 
