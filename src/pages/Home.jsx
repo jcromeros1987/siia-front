b3d810508203315206b464f6beb9e3da72934347
geromt <gerardomartinez205@gmail.com>
2026-02-23 12:41:16 -0600

    feat: update carousel navigation in Home component for better user experience

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,9 +16,12 @@ const Home = () => {
         <div className='flex-1 overflow-y-auto w-full'>
           <UserInfo userData={userData} isLoading={isLoading} />
         </div>
-        <div className='absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between pointer-events-none'>
+        <div className='absolute left-5 right-5 top-1/2 hidden md:flex -translate-y-1/2 transform justify-between pointer-events-none'>
           <a href='#cvuInfo' className='btn btn-circle pointer-events-auto'>❮</a>
           <a href='#cvuInfo' className='btn btn-circle pointer-events-auto'>❯</a>
+        </div>
+        <div className='absolute bottom-4 left-0 right-0 md:hidden flex justify-center'>
+          <span className='badge badge-neutral text-xs'>Desliza para más →</span>
         </div>
       </div>
 
@@ -26,9 +29,12 @@ const Home = () => {
         <div className='flex-1 overflow-y-auto w-full'>
           <CVUInfo cvuData={cvuData} fetchCVUData={fetchCVUData} isLoading={isLoading} />
         </div>
-        <div className='absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between pointer-events-none z-50'>
+        <div className='absolute left-5 right-5 top-1/2 hidden md:flex -translate-y-1/2 transform justify-between pointer-events-none z-50'>
           <a href='#userInfo' className='btn btn-circle pointer-events-auto'>❮</a>
           <a href='#userInfo' className='btn btn-circle pointer-events-auto'>❯</a>
+        </div>
+        <div className='absolute bottom-4 left-0 right-0 md:hidden flex justify-center'>
+          <span className='badge badge-neutral text-xs'>← Desliza para atrás</span>
         </div>
       </div>
 
