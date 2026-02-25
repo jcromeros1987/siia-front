01951838d45693b8a3644fc1a278492d11a78567
geromt <gerardomartinez205@gmail.com>
2026-02-24 19:58:29 -0600

    feat: add error handling to fetchCVUData for improved reliability

--- src/hooks/useFetchCVU.js
+++ src/hooks/useFetchCVU.js
@@ -7,12 +7,14 @@ export const useFetchCVU = () => {
   const [cvuData, setCvuData] = useState({})
   const [userData, setUserData] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const fetchCVUData = ({ skipCache = false } = {}) => {
     if (!skipCache && cvuData && Object.keys(cvuData).length > 0 && userData && Object.keys(userData).length > 0) {
       return
     }
     setIsLoading(true)
+    setError(null)
     fetchCVU({ token, onTokenRefresh: updateAccessToken, userId })
       .then((response) => {
         const data = response.data.data || {}
@@ -20,10 +22,16 @@ export const useFetchCVU = () => {
 
         setCvuData(data)
         setUserData(userData)
+        setError(null)
       })
-      .catch((error) => console.error('Error fetching CVU data:', error))
+      .catch((error) => {
+        console.error('Error fetching CVU data:', error)
+        setError(error.response?.status)
+        setCvuData({})
+        setUserData({})
+      })
       .finally(() => setIsLoading(false))
   }
 
-  return { cvuData, userData, fetchCVUData, isLoading }
+  return { cvuData, userData, fetchCVUData, isLoading, error }
 }
